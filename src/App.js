86c4d7cddff2450e9c7756c2ac9fb672774d78d7
Andy Amaya <aamay001@gmail.com>
2017-09-21 12:32:15 -0700

    Setup links list for drills.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const branchList = this.props.branches.map(branch =>
+      <li><a href={branch.url}>{branch.name}</a></li>
+    );
     return (
-      <h1>See branches for drills.</h1>
+      <div>
+        <h1>See branches for drills.</h1>
+        <ul>{branchList}</ul>
+      </div>
     );
   }
+}
+
+App.defaultProps = {
+  branches: [
+    {
+      name: "Navigation Bar",
+      url: "https://github.com/aamay001/react-component-drills/tree/learn/navigation-bar"
+    }
+  ]
 }
 
 export default App;
